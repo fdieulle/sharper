20d7df12de9a8f450f102747033d2227b4afba15
fdieulle <fabien.dieulle@gmail.com>
2018-05-10 13:30:59 +0200

    Add zzz.R file for package entry point when it's loading

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+# sharper .onLoad.
+# 
+# Function called when the sharper package is loading. 
+# 
+# @param pkgsDir package folder where the library is stored and loaded.
+# @param pkgName package name.
+# @rdname dotOnLoad
+# @name dotOnLoad
+.onLoad <- function(pkgsDir='~/R', pkgName = 'r.net') {
+
+}
