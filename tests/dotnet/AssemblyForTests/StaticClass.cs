3562ffabd960360e338a9be0defbb042e3f5cc6a
fdieulle <fabien.dieulle@gmail.com>
2019-01-30 19:46:50 +0100

    Introduce tests project and add a first test on TryGetMethod function

---
+++ tests/dotnet/AssemblyForTests/StaticClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace AssemblyForTests
+{
+    public static class StaticClass
+    {
+        #region Same method name
+
+        public static void SameMethodName()
+        {
+            Console.WriteLine("SameMethodName() Call");
+        }
+
+        public static void SameMethodName(int intValue)
+        {
+            Console.WriteLine("SameMethodName(int) arg: {0}", intValue);
+        }
+
+        public static void SameMethodName(double doubleValue)
+        {
+            Console.WriteLine("SameMethodName(double) arg: {0}", doubleValue);
+        }
+
+        public static void SameMethodName(int[] intValue)
+        {
+            Console.WriteLine("SameMethodName(int[]) arg: {0}", string.Join(";", intValue));
+        }
+
+        public static void SameMethodName(double[] doubleValue)
+        {
+            Console.WriteLine("SameMethodName(double[]) arg: {0}", string.Join(";", doubleValue));
+        }
+
+        public static void SameMethodName(double doubleValue, int intValue)
+        {
+            Console.WriteLine("SameMethodName(double, int) arg1: {0}, arg2: {1}", doubleValue, intValue);
+        }
+
+        public static void SameMethodName(double[] doubleValue, int[] intValue)
+        {
+            Console.WriteLine("SameMethodName(double[], int[]) arg1: {0}, arg2: {1}", string.Join(";", doubleValue), string.Join(";", intValue));
+        }
+
+        public static void SameMethodName(double[] doubleValue, int intValue)
+        {
+            Console.WriteLine("SameMethodName(double[], int) arg1: {0}, arg2: {1}", string.Join(";", doubleValue), intValue);
+        }
+
+        public static void SameMethodName(double doubleValue, int[] intValue)
+        {
+            Console.WriteLine("SameMethodName(double, int[]) arg1: {0}, arg2: {1}", doubleValue, string.Join(";", intValue));
+        }
+
+        #endregion
+    }
+}
---
+++ tests/dotnet/Sharper.Tests/ClrProxyTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+using NSubstitute;
+using NUnit.Framework;
+using Sharper.Converters;
+
+namespace Sharper.Tests
+{
+    [TestFixture]
+    public class ClrProxyTests
+    {
+#if DEBUG
+        private const string PATH = @"..\..\..\..\AssemblyForTests\bin\Debug\netstandard2.0\AssemblyForTests.dll";
+#elif RELEASE
+        private const string PATH = @"..\..\..\..\AssemblyForTests\bin\Release\netstandard2.0\AssemblyForTests.dll";
+#endif
+
+        [Test]
+        public void TestGetMethod()
+        {
+            ClrProxy.DataConverter = Substitute.For<IDataConverter>();
+            Assert.IsNotNull(ClrProxy.LoadAssembly(PATH));
+
+            var typeName = "AssemblyForTests.StaticClass";
+            var methodName = "SameMethodName";
+            const BindingFlags flags = BindingFlags.Public | BindingFlags.Static;
+
+            ClrProxy.CallStaticMethod(typeName, methodName, null, 0);
+            
+            Assert.IsTrue(typeName.TryGetType(out var type, out var errorMsg));
+            
+            Assert.IsTrue(type.TryGetMethod(methodName, flags, A(C<int>()), out var method1));
+            Assert.IsTrue(type.TryGetMethod(methodName, flags, A(C<double>()), out var method2));
+            Assert.IsTrue(type.TryGetMethod(methodName, flags, A(C<int[]>()), out var method3));
+            Assert.IsTrue(type.TryGetMethod(methodName, flags, A(C<double[]>()), out var method4));
+            Assert.IsTrue(type.TryGetMethod(methodName, flags, A(C<double>(), C<int>()), out var method5));
+            Assert.IsTrue(type.TryGetMethod(methodName, flags, A(C<double[]>(), C<int[]>()), out var method6));
+            Assert.IsTrue(type.TryGetMethod(methodName, flags, A(C<double[]>(), C<int>()), out var method7));
+            Assert.IsTrue(type.TryGetMethod(methodName, flags, A(C<double>(), C<int[]>()), out var method8));
+        }
+
+        private static IConverter C(Type type)
+        {
+            var converter = Substitute.For<IConverter>();
+            converter.GetClrTypes().Returns(new[] {type});
+            return converter;
+        }
+
+        private static IConverter C<T>() => C(typeof(T));
+
+        private static T[] A<T>(params T[] array) => array;
+    }
+}
