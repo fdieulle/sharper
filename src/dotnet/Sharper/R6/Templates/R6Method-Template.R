27150d620304412df38c7a7523b557509c7fe5ac
fdieulle <fabien.dieulle@gmail.com>
2019-12-16 15:17:46 -0500

    Fix parameters given by reference on inherited R6 class generator

--- src/dotnet/Sharper/R6/Templates/R6Method-Template.R
+++ src/dotnet/Sharper/R6/Templates/R6Method-Template.R
@@ -2,6 +2,17 @@
 #' {{Description}}
 #' {{Parameters_doc}}
 #' @return {{Return_doc}}
-{{MethodName}} = function ({{Parameters}}) {
-  {{Return}} (self$call("{{MethodName}}"{{Comma}}{{Parameters}}))
+{{MethodName}} = function ({{Parameters}}{{Comma}}wrap = TRUE, out_env = parent.frame()) {
+  if ({{HasByRefParams}}) {
+    Call <- match.call()
+	result <- self$call("{{MethodName}}", {{Parameters}}, wrap = wrap)
+	call_names <- names(Call)
+	for (i in 2:length(Call)) {
+		if (call_names[i] == "wrap" || call_names[i] == "out_env") next
+		assign(deparse(Call[[i]]), get(call_names[i]), env = out_env)
+	}
+	{{Return}} (result)
+  } else {
+    {{Return}} (self$call("{{MethodName}}"{{Comma}}{{Parameters}}, wrap = wrap, out_env = out_env))
+  }
 }
